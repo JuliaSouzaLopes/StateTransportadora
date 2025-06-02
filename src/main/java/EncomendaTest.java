a8ab759fe565674e781fcbb65a38a5bcd7019c84
Julia Lopes <juliasouzavieiralopes@gmail.com>
2025-06-02 19:34:49 -0300

    Testes

--- src/main/java/EncomendaTest.java
+++ src/main/java/EncomendaTest.java
@@ -1,5 +1,4 @@
-import org.example.Encomenda;
-import org.example.EncomendarEstadoRecebida;
+import org.example.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +18,56 @@ public class EncomendaTest {
         encomenda.setEstado(EncomendarEstadoRecebida.getInstance());
         assertFalse(encomenda.receber());
     }
+
+    @Test
+    public void deveSepararEncomendaRecebida() {
+        encomenda.setEstado(EncomendarEstadoRecebida.getInstance());
+        assertTrue(encomenda.separar());
+        assertEquals(EncomendaEstadoSeparada.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveCancelarEncomendaRecebida() {
+        encomenda.setEstado(EncomendarEstadoRecebida.getInstance());
+        assertTrue(encomenda.cancelar());
+        assertEquals(EncomendaEstadoCancelada.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveTransportatEncomendaSeparada() {
+        encomenda.setEstado(EncomendarEstadoRecebida.getInstance());
+        assertTrue(encomenda.transportar());
+        assertEquals(EncomendaEstadoTransporte.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveCorreiosEncomendaTransporte(){
+        encomenda.setEstado(EncomendaEstadoTransporte.getInstance());
+        assertTrue(encomenda.correios());
+        assertEquals(EncomendaEstadoCorreios.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveEntregarEncomendaTransporte(){
+        encomenda.setEstado(EncomendaEstadoTransporte.getInstance());
+        assertTrue(encomenda.entregar());
+        assertEquals(EncomendaEstadoEntregue.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveDevolverEncomendaCorreios(){
+        encomenda.setEstado(EncomendaEstadoCorreios.getInstance());
+        assertTrue(encomenda.devolver());
+        assertEquals(EncomendaEstadoDevolvida.getInstance(),encomenda.getEstado());
+    }
+
+    @Test
+    public void deveDevolverEncomendaEntregue(){
+        encomenda.setEstado(EncomendaEstadoEntregue.getInstance());
+        assertTrue(encomenda.devolver());
+        assertEquals(EncomendaEstadoDevolvida.getInstance(),encomenda.getEstado());
+    }
+
+
 
 }
